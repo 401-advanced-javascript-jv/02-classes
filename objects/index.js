c076e8c51d0fcb63422c28b148032b1e180b7c9f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-05-14 15:00:52 -0700

    added some tests

--- objects/index.js
+++ objects/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VehicleConstructor = require('./vehicle-class.js');
+const VehicleConstructor = require('./vehicle-constructor.js');
 
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
@@ -10,6 +10,12 @@ const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
 // Implement a car and motorcycle using a Class
+const VehicleClass = require('./vehicle-constructor.js');
 
+const camry = new VehicleClass.Car('Camry');
+console.log(camry.name, camry.drive(), camry.stop());
+
+const ducati = new VehicleClass.Motorcycle('Ducati');
+console.log(ducati.name, ducati.drive(), ducati.wheelie(), ducati.stop());
 
 // Implement a car and motorcycle using a Factory
